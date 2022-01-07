99a3ad0ec7b9827fcd7ceaf6a927bb377f66b385
V Anastassiou <vicky.anastassiou@slalom.com>
2022-01-07 12:56:30 -0800

    Implement check for site changes

--- scripts/back-up-website.sh
+++ scripts/back-up-website.sh
@@ -2,6 +2,8 @@
 #
 # Backs up the sitefiles and MySQL database for a WordPress website to an
 # AWS S3 bucket. Files are archived to reduce S3's per-request charges.
+# Currently takes a full backup only if changes are detected. A future 
+# incremental backup strategy will likely rely on AWS's managed services.
 
 TIMESTAMP="$(date '+%Y-%m-%d_%H:%M:%S')"
 TEMP_DIR="/tmp"
@@ -41,16 +43,31 @@ WP_DB_HOST=$(cat "${WP_CONFIG_FILE}" | grep -Po "DB_HOST', '\\K.*(?=')")
 ## Use ~/.my.cnf instead of CLI auth, set up during restore-website.sh
 mysqldump -u "${WP_DB_USER}" -h "${WP_DB_HOST}" "${WP_DB_NAME}" > "${TEMP_DIR}/${WEBSITE_DB_DUMP}"
 
+# Compare sizes of newly created zipfile and dumpfile against most recently
+# retrieved backups, which were downloaded as part of restore.sh. Clean up and
+# exit if sizes are identical.
+
+BACKUPS_DIR="/home/ubuntu/backups"
+OLD_SITEFILES_ZIP_SIZE=$(stat "${BACKUPS_DIR}/${WEBSITE_DOMAIN}"*.zip --format="%s")
+OLD_WEBSITE_DB_DUMP_SIZE=$(stat "${BACKUPS_DIR}/${WEBSITE_DOMAIN}"*.dump --format="%s")
+
+NEW_SITEFILES_ZIP_SIZE=$(stat "${TEMP_DIR}/${SITEFILES_ZIP}" --format="%s")
+NEW_WEBSITE_DB_DUMP_SIZE=$(stat "${TEMP_DIR}/${WEBSITE_DB_DUMP}" --format="%s")
+
+if [ ${OLD_SITEFILES_ZIP_SIZE} -eq ${NEW_SITEFILES_ZIP_SIZE} && ${OLD_WEBSITE_DB_DUMP_SIZE} -eq ${NEW_WEBSITE_DB_DUMP_SIZE} ]; then
+  echo "INFO: site does not appear to have been changed since last restore. Cancelling backup."
+  rm "${TEMP_DIR}/${SITEFILES_ZIP}" "${TEMP_DIR}/${WEBSITE_DB_DUMP}"
+  exit $?
+fi
+
 # Transfer files to S3 bucket. AWS CLI install/S3 user config not required,
 # as these were done during provisioning in restore-website.sh
 aws s3 cp "${TEMP_DIR}/${SITEFILES_ZIP}" s3://"${S3_BUCKET_NAME}"/
 aws s3 cp "${TEMP_DIR}/${WEBSITE_DB_DUMP}" s3://"${S3_BUCKET_NAME}"/
 
 # Verify transfer to S3 by checking filesizes
-AWS_SITEFILES_ZIP_SIZE=$(aws s3api head-object --bucket=${S3_BUCKET_NAME} --key "${SITEFILES_ZIP}" --query 'ContentLength')
-AWS_WEBSITE_DB_DUMP_SIZE=$(aws s3api head-object --bucket=${S3_BUCKET_NAME} --key "${WEBSITE_DB_DUMP}" --query 'ContentLength')
-LOCAL_SITEFILES_ZIP_SIZE=$(stat ${TEMP_DIR}/${SITEFILES_ZIP} --format="%s")
-LOCAL_WEBSITE_DB_DUMP_SIZE=$(stat ${TEMP_DIR}/${WEBSITE_DB_DUMP} --format="%s")
+AWS_SITEFILES_ZIP_SIZE=$(aws s3api head-object --bucket=${S3_BUCKET_NAME} --key "${NEWEST_AWS_SITEFILES_BACKUP}" --query 'ContentLength')
+AWS_WEBSITE_DB_DUMP_SIZE=$(aws s3api head-object --bucket=${S3_BUCKET_NAME} --key "${NEWEST_AWS_DUMP_BACKUP}" --query 'ContentLength')
 
 if [ $AWS_SITEFILES_ZIP_SIZE != ${LOCAL_SITEFILES_ZIP_SIZE} ]; then
   echo "ERROR: file size mismatch for ${SITEFILES_ZIP} after upload to S3"
