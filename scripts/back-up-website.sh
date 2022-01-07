13e57c2c545652d773986d588e22a6ad2d4820d5
V Anastassiou <vicky.anastassiou@slalom.com>
2022-01-07 13:49:35 -0800

    Fix syntax and add communication of script status

--- scripts/back-up-website.sh
+++ scripts/back-up-website.sh
@@ -4,6 +4,9 @@
 # AWS S3 bucket. Files are archived to reduce S3's per-request charges.
 # Currently takes a full backup only if changes are detected. A future 
 # incremental backup strategy will likely rely on AWS's managed services.
+#
+# Terraform uploads this script to the EC2 instance upon creation, and
+# restore-website.sh configures the appropriate cron job for a nightly run.
 
 TIMESTAMP="$(date '+%Y-%m-%d_%H:%M:%S')"
 TEMP_DIR="/tmp"
@@ -15,19 +18,21 @@ S3_BUCKET_NAME="omnomnomica-backups"
 # Add site files and Apache virtual host config files to one zip archive
 SITEFILES_ZIP="${WEBSITE_DOMAIN}-${TIMESTAMP}.zip"
 
+echo "INFO: Creating archive of website document root and virtual hosts files"
+
 cd ${WEBSITE_ROOT}
-zip -r "${TEMP_DIR}/${SITEFILES_ZIP}" public_html
+zip -r "${TEMP_DIR}/${SITEFILES_ZIP}" public_html >/dev/null 2>&1
 
 cd ${VHOST_FILE_LOCATION}
-zip -u "${TEMP_DIR}/${SITEFILES_ZIP}" ${WEBSITE_DOMAIN}*
+zip -u "${TEMP_DIR}/${SITEFILES_ZIP}" "${WEBSITE_DOMAIN}"* >/dev/null 2>&1
 
-# Check zipfile integrity
+## Check zipfile integrity
 
-echo "INFO: testing zipfile integrity"
+echo "INFO: Testing archive integrity"
 unzip -t "${TEMP_DIR}/${SITEFILES_ZIP}" >/dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-  echo "SUCCESS: ${SITEFILES_ZIP} archive integrity verified"
+  echo "SUCCESS: Verified integrity of ${SITEFILES_ZIP} archive"
 else
   echo "ERROR: ${SITEFILES_ZIP} failed its integrity check"
   exit $?
@@ -41,24 +46,29 @@ WP_DB_NAME=$(cat "${WP_CONFIG_FILE}" | grep -Po "DB_NAME', '\\K.*(?=')")
 WP_DB_HOST=$(cat "${WP_CONFIG_FILE}" | grep -Po "DB_HOST', '\\K.*(?=')")
 
 ## Use ~/.my.cnf instead of CLI auth, set up during restore-website.sh
-mysqldump -u "${WP_DB_USER}" -h "${WP_DB_HOST}" "${WP_DB_NAME}" > "${TEMP_DIR}/${WEBSITE_DB_DUMP}"
+echo "INFO: Dumping ${WP_DB_NAME} database to file"
+sudo mysqldump "${WP_DB_NAME}" > "${TEMP_DIR}/${WEBSITE_DB_DUMP}"
+echo "SUCCESS: Dump complete"
 
 # Compare sizes of newly created zipfile and dumpfile against most recently
 # retrieved backups, which were downloaded as part of restore.sh. Clean up and
 # exit if sizes are identical.
+echo "INFO: Comparing new backup files to old"
 
 BACKUPS_DIR="/home/ubuntu/backups"
 OLD_SITEFILES_ZIP_SIZE=$(stat "${BACKUPS_DIR}/${WEBSITE_DOMAIN}"*.zip --format="%s")
-OLD_WEBSITE_DB_DUMP_SIZE=$(stat "${BACKUPS_DIR}/${WEBSITE_DOMAIN}"*.dump --format="%s")
+OLD_WEBSITE_DB_DUMP_SIZE=$(stat "${BACKUPS_DIR}/${WEBSITE_DOMAIN}"*.sql --format="%s")
 
 NEW_SITEFILES_ZIP_SIZE=$(stat "${TEMP_DIR}/${SITEFILES_ZIP}" --format="%s")
 NEW_WEBSITE_DB_DUMP_SIZE=$(stat "${TEMP_DIR}/${WEBSITE_DB_DUMP}" --format="%s")
 
-if [ ${OLD_SITEFILES_ZIP_SIZE} -eq ${NEW_SITEFILES_ZIP_SIZE} && ${OLD_WEBSITE_DB_DUMP_SIZE} -eq ${NEW_WEBSITE_DB_DUMP_SIZE} ]; then
-  echo "INFO: site does not appear to have been changed since last restore. Cancelling backup."
+if [ ${OLD_SITEFILES_ZIP_SIZE} -eq ${NEW_SITEFILES_ZIP_SIZE} ] && [ ${OLD_WEBSITE_DB_DUMP_SIZE} -eq ${NEW_WEBSITE_DB_DUMP_SIZE} ]; then
+  echo "ERROR: No changes detected to website since last restore. Cancelling backup."
   rm "${TEMP_DIR}/${SITEFILES_ZIP}" "${TEMP_DIR}/${WEBSITE_DB_DUMP}"
   exit $?
 fi
+
+echo "SUCCESS: New files different from old; proceeding with upload to ${S3_BUCKET_NAME}"
 
 # Transfer files to S3 bucket. AWS CLI install/S3 user config not required,
 # as these were done during provisioning in restore-website.sh
@@ -70,9 +80,13 @@ AWS_SITEFILES_ZIP_SIZE=$(aws s3api head-object --bucket=${S3_BUCKET_NAME} --key 
 AWS_WEBSITE_DB_DUMP_SIZE=$(aws s3api head-object --bucket=${S3_BUCKET_NAME} --key "${NEWEST_AWS_DUMP_BACKUP}" --query 'ContentLength')
 
 if [ $AWS_SITEFILES_ZIP_SIZE != ${LOCAL_SITEFILES_ZIP_SIZE} ]; then
-  echo "ERROR: file size mismatch for ${SITEFILES_ZIP} after upload to S3"
+  echo "WARNING: file size mismatch for ${SITEFILES_ZIP} after upload to S3.
+  The file has been uploaded, but you should verify it."
 fi
 
 if [ $AWS_WEBSITE_DB_DUMP_SIZE != ${LOCAL_WEBSITE_DB_DUMP_SIZE} ]; then
-  echo "ERROR: file size mismatch for ${WEBSITE_DB_DUMP} after upload to S3"
+  echo "WARNING: file size mismatch for ${WEBSITE_DB_DUMP} after upload to S3.
+  The file has been uploaded, but you should verify it."
 fi
+
+echo "SUCCESS: Backup complete"
