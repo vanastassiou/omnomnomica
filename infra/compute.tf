3dc2c8574d26a7c5ce01d0b3de443acee66623f3
V Anastassiou <vicky.anastassiou@slalom.com>
2022-01-24 14:50:26 -0800

    Use correct SG resource

--- infra/compute.tf
+++ infra/compute.tf
@@ -9,37 +9,22 @@ resource "aws_key_pair" "deployer_public_key" {
 resource "aws_vpc" "default" {
 }
 
-resource "aws_security_group" "default" {
-  name        = "default"
-  description = "default VPC security group"
-  vpc_id      = aws_vpc.default.id
-  egress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    }
-  ]
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.default.id
 
-  ingress = [
-    {
-      cidr_blocks      = []
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = true
-      to_port          = 0
-    }
-  ]
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "webserver" {
