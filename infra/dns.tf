c620e844653e88881ceb04968656ecab0602a52d
V Anastassiou <vicky.anastassiou@slalom.com>
2022-01-24 14:39:46 -0800

    Import existing record

--- infra/dns.tf
+++ infra/dns.tf
@@ -6,6 +6,6 @@ resource "aws_route53_record" "prod" {
   zone_id = aws_route53_zone.prod.zone_id
   name    = var.base_domain_name
   type    = "A"
-  ttl     = "30"
+  ttl     = "300"
   records = [aws_instance.web.public_ip]
 }
